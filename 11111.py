73744be8e9e49af4e68ff0a60c85000113d1f8c5
yifanyuan910-rgb <yifanyuan910@gmail.com>
2026-04-11 15:37:51 +0800

    xiugai

--- 11111.py
+++ 11111.py
@@ -1,3 +1,29 @@
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
+import torch
+from torch import nn
+from torch import Parameter
+
+class MyMultiheadAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads, dropout=0., bias=True):
+        super(MyMultiheadAttention, self).__init__()
+        """
+        :param embed_dim:   词嵌入的维度，也就是前面的d_model参数，论文中的默认值为512
+        :param num_heads:   多头注意力机制中多头的数量，也就是前面的nhead参数， 论文默认值为 8
+        :param bias:        最后对多头的注意力（组合）输出进行线性变换时，是否使用偏置
+        """
+        self.embed_dim = embed_dim  # 前面的d_model参数
+        self.head_dim = embed_dim // num_heads  # head_dim 指的就是d_k,d_v
+        self.kdim = self.head_dim
+        self.vdim = self.head_dim
+        self.num_heads = num_heads  # 多头个数
+        self.dropout = dropout
+        assert self.head_dim * num_heads == self.embed_dim, "embed_dim 除以 num_heads必须为整数"
+        # 上面的限制条件就是论文中的  d_k = d_v = d_model/n_head 条件
+        self.q_proj_weight = Parameter(torch.Tensor(embed_dim, embed_dim))  
+        # embed_dim = kdim * num_heads
+        # 这里第二个维度之所以是embed_dim，实际上这里是同时初始化了num_heads个W_q堆叠起来的, 也就是num_heads个头
+        self.k_proj_weight = Parameter(torch.Tensor(embed_dim, embed_dim))  
+        # W_k,  embed_dim = kdim * num_heads
+        self.v_proj_weight = Parameter(torch.Tensor(embed_dim, embed_dim))  
+        # W_v,  embed_dim = vdim * num_heads
+        self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        # 最后将所有的Z组合起来的时候，也是一次性完成， embed_dim = vdim * num_heads
